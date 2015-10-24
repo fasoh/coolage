706d701ed6433fb764fb8806477995ffe403e688
Jannik <jannikspieker@icloud.com>
2015-10-24 17:24:59 +0200

    Testmethode eingefügt: Lesen und Anzeigen von Bild aus URL

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,49 @@
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
+        Image img = loadImageFromURL();
+        displayImage(img);
+    }
+
+    private static Image loadImageFromURL() {
+        URL imageURL = null;
+        Image img = null;
+
+        try {
+            imageURL = new URL("http://i.imgur.com/MY47ivh.jpg");
+            img = ImageIO.read(imageURL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return img;
+    }
+
+    private static void displayImage(Image img) {
+        ImageIcon icon = new ImageIcon(img);
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(img.getWidth(null)+50, img.getHeight(null)+50);
+        JLabel lbl = new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private static void opencvTest() {
         System.out.println("Welcome to OpenCV " + Core.VERSION);
         Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
         System.out.println("OpenCV Mat: " + m);
