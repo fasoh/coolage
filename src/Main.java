9f4a8553576e1a02a6a5d420d39aae37f732b6e9
Jannik <jannikspieker@icloud.com>
2015-10-25 18:13:59 +0100

    DetectFaceDemo liest nun Bilder aus URL, konvertiert diese automatisch in BGR und in Mat und erkennt dann Gesichter

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 import org.opencv.core.*;
+import org.opencv.core.Point;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Load the native library.
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        new DetectFaceDemo().run();
+    }
+}
+
 
 //
 // Detects faces in an image, draws boxes around them, and writes the results
@@ -14,7 +34,9 @@ class DetectFaceDemo {
         // Create a face detector from the cascade file in the resources
         // directory.
         CascadeClassifier faceDetector = new CascadeClassifier(System.getProperty("user.dir") + "/src/lbpcascade_frontalface.xml");
-        Mat image = Imgcodecs.imread(System.getProperty("user.dir") + "/src/lena.png");
+        BufferedImage buffImage = loadImageFromURL("http://i.imgur.com/nNAM4vu.jpg");
+        Mat image = convertBufferedToMat(buffImage);
+
 
         // Detect faces in the image.
         // MatOfRect is a special container class for Rect.
@@ -32,15 +54,67 @@ class DetectFaceDemo {
         String filename = "faceDetection.png";
         System.out.println(String.format("Writing %s", filename));
         Imgcodecs.imwrite(filename, image);
+        System.out.println("Image saved under " + System.getProperty("user.dir") + "/" + filename);
     }
-}
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, OpenCV");
+    private BufferedImage loadImageFromURL(String urlString) {
+        URL imageURL = null;
+        BufferedImage img = null;
+        System.out.print("Downloading picture from URL " + urlString + " ... ");
 
-        // Load the native library.
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        new DetectFaceDemo().run();
+        try {
+            imageURL = new URL(urlString);
+            //Convert to BufferedImage of type BGR so openCV can read it
+            img = toBufferedImageOfType(ImageIO.read(imageURL), BufferedImage.TYPE_3BYTE_BGR);
+            System.out.println("Success!");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return img;
     }
+
+    public static BufferedImage toBufferedImageOfType(BufferedImage original, int type) {
+
+        if (original == null) {
+            throw new IllegalArgumentException("original == null");
+        }
+
+        // Don't convert if it already has correct type
+        if (original.getType() == type) {
+            return original;
+        }
+
+        // Create a buffered image
+        BufferedImage image = new BufferedImage(original.getWidth(), original.getHeight(), type);
+
+        // Draw the image onto the new buffer
+        Graphics2D g = image.createGraphics();
+        try {
+            g.setComposite(AlphaComposite.Src);
+            g.drawImage(original, 0, 0, null);
+        } finally {
+            g.dispose();
+        }
+
+        return image;
+    }
+
+    public Mat convertBufferedToMat(BufferedImage buffImage) {
+        System.out.print("Converting Image to Mat ... ");
+        //Convert BufferedImage from URL to Mat for OpenCV
+        byte[] pixels = ((DataBufferByte) buffImage.getRaster().getDataBuffer()).getData();
+        // Create a Matrix the same size of image
+        Mat imageMat = new Mat(buffImage.getHeight(), buffImage.getWidth(), CvType.CV_8UC3);
+        // Fill Matrix with image values
+        imageMat.put(0, 0, pixels);
+
+        System.out.println("Success!");
+        return imageMat;
+    }
+
+
+
 }
