9be4dc2e5dda93242d16c25c3a1ce6ec26102ee4
Adrian <adrian.jagusch@uni-oldenburg.de>
2015-10-25 15:59:54 +0100

    Funktionierende Gesichtserkennung

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,46 @@
 import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+//
+// Detects faces in an image, draws boxes around them, and writes the results
+// to "faceDetection.png".
+//
+class DetectFaceDemo {
+    public void run() {
+        System.out.println("\nRunning DetectFaceDemo");
 
-public class Main {
-    public static void main(String[] args){
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        // Create a face detector from the cascade file in the resources
+        // directory.
+        CascadeClassifier faceDetector = new CascadeClassifier(System.getProperty("user.dir") + "/src/lbpcascade_frontalface.xml");
+        Mat image = Imgcodecs.imread(System.getProperty("user.dir") + "/src/lena.png");
 
-        Image img = loadImageFromURL();
-        displayImage(img);
-    }
+        // Detect faces in the image.
+        // MatOfRect is a special container class for Rect.
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(image, faceDetections);
 
-    private static Image loadImageFromURL() {
-        URL imageURL = null;
-        Image img = null;
+        System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
 
-        try {
-            imageURL = new URL("http://i.imgur.com/MY47ivh.jpg");
-            img = ImageIO.read(imageURL);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        // Draw a bounding box around each face.
+        for (Rect rect : faceDetections.toArray()) {
+            Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
         }
 
-        return img;
+        // Save the visualized detection.
+        String filename = "faceDetection.png";
+        System.out.println(String.format("Writing %s", filename));
+        Imgcodecs.imwrite(filename, image);
     }
+}
 
-    private static void displayImage(Image img) {
-        ImageIcon icon = new ImageIcon(img);
-        JFrame frame = new JFrame();
-        frame.setLayout(new FlowLayout());
-        frame.setSize(img.getWidth(null)+50, img.getHeight(null)+50);
-        JLabel lbl = new JLabel();
-        lbl.setIcon(icon);
-        frame.add(lbl);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, OpenCV");
 
-    private static void opencvTest() {
-        System.out.println("Welcome to OpenCV " + Core.VERSION);
-        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-        System.out.println("OpenCV Mat: " + m);
-        Mat mr1 = m.row(1);
-        mr1.setTo(new Scalar(1));
-        Mat mc5 = m.col(5);
-        mc5.setTo(new Scalar(5));
-        System.out.println("OpenCV Mat data:\n" + m.dump());
+        // Load the native library.
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        new DetectFaceDemo().run();
     }
 }
