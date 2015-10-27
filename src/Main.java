2bb5384b117d9eede4251d1c4de26f8e8fd53351
Jannik <jannikspieker@icloud.com>
2015-10-27 18:21:57 +0100

    Mehrere Gesichter können erkannt werden und werden zu einem Bild gestitched

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
         ImageProcessor imageProcessor = new ImageProcessor();
 
         imageProcessor.detectFace("http://i.imgur.com/nNAM4vu.jpg");
+        imageProcessor.detectFace("http://i.imgur.com/yAY2OnG.jpg");
+        imageProcessor.detectFace("http://i.imgur.com/IeC86Kx.jpg");
+
         imageProcessor.drawLettersOnGeneratedImage("ABC", Color.WHITE); //(text, backgroundColor)
 
     }
