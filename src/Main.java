6c96c3e1aa0b88e73dae4d515ffdc56238eefa9f
Jannik <jannikspieker@icloud.com>
2016-01-03 23:36:53 +0100

    Tresholding verbessert, brauchbar gemacht und eingebaut; Kleinere Bugfixes und Verschönerungen; Neue Bilder (zum Testen) eingefügt

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,13 @@ public class Main {
         Dimension boundary = new Dimension(620, 414);
 
         ArrayList<String> urlList = new ArrayList<String>();
-        urlList.add("http://lh5.ggpht.com/fkOpCsPe9PNSEjcCzmWpbHbtOJ3MuMCujcj9jWmAwrE3KwoXTjQb5Eq4IGOSgWO2fEf9HP2rceZgh1d7=s620");
-        urlList.add("http://knowingtechnologies.com/wp-content/uploads/2015/06/iStock_000046819354_Medium-600x400.jpg");
-        urlList.add("http://lh4.ggpht.com/8MCTX5uAr5bJxXzVdkk-vETHKTHQQZVp9dl9xqaaBNcTfvjkl8P05elW7f1aSf5Gd_ewxs4Wpgcjqb-Xfsk=s620");
+        urlList.add("https://i.imgur.com/FEPDZ8M.jpg");
+        urlList.add("http://imgur.com/g6zYCMW.jpg");
+        urlList.add("http://i.imgur.com/a2F7iyp.jpg");
         urlList.add("http://blogs.reuters.com/great-debate/files/2013/07/obama-best.jpg");
-        urlList.add("http://stockfresh.com/files/g/goce/m/98/5167439_stock-photo-green-barley-field-nature-background.jpg");
+        urlList.add("https://i.imgur.com/OQuArU4.jpg");
         String fontUrl = "https://fonts.gstatic.com/s/raleway/v9/PKCRbVvRfd5n7BTjtGiFZMDdSZkkecOE1hvV7ZHvhyU.ttf";
-        String text = "testtesttest";
+        String text = "abcde";
         //TODO Für leerzeichen muss das bild nicht geladen werden (im converter) (bsp "a b c" lädt 5 bilder runter, nicht 3)
 
         ImageProcessor imageProcessor = new ImageProcessor(fontUrl, 400f, Color.WHITE, 2f, Color.BLACK, 15);
