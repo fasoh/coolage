252aeb876492fe69357f74bc5d489a19d60ec271
Adrian <adrian.jagusch@uni-oldenburg.de>
2016-01-12 00:12:52 +0100

    Bilder werden übernommen

--- src/main/java/de/uniOldenburg/presenter/CoolageServlet.java
+++ src/main/java/de/uniOldenburg/presenter/CoolageServlet.java
@@ -25,11 +25,13 @@ public class CoolageServlet extends HttpServlet {
         Dimension boundary = new Dimension(620, 414);
 
         ArrayList<String> urlList = new ArrayList<String>();
-        urlList.add("http://lh5.ggpht.com/fkOpCsPe9PNSEjcCzmWpbHbtOJ3MuMCujcj9jWmAwrE3KwoXTjQb5Eq4IGOSgWO2fEf9HP2rceZgh1d7=s620");
-        urlList.add("http://knowingtechnologies.com/wp-content/uploads/2015/06/iStock_000046819354_Medium-600x400.jpg");
-        urlList.add("http://lh4.ggpht.com/8MCTX5uAr5bJxXzVdkk-vETHKTHQQZVp9dl9xqaaBNcTfvjkl8P05elW7f1aSf5Gd_ewxs4Wpgcjqb-Xfsk=s620");
-        urlList.add("http://blogs.reuters.com/great-debate/files/2013/07/obama-best.jpg");
-        urlList.add("http://stockfresh.com/files/g/goce/m/98/5167439_stock-photo-green-barley-field-nature-background.jpg");
+
+        String[] imagesArray = allImages.split(";");
+
+        for (String image : imagesArray) {
+            urlList.add(image);
+        }
+
         String fontUrl = "https://fonts.gstatic.com/s/raleway/v9/PKCRbVvRfd5n7BTjtGiFZMDdSZkkecOE1hvV7ZHvhyU.ttf";
         //TODO Für leerzeichen muss das bild nicht geladen werden (im converter) (bsp "a b c" lädt 5 bilder runter, nicht 3)
 
@@ -41,7 +43,6 @@ public class CoolageServlet extends HttpServlet {
         }
 
         System.out.println("Done!");
-
 
         response.getWriter().write(allImages);
     }
